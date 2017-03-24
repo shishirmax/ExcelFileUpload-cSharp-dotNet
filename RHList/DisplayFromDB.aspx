415ed058a3ac87645598ff3479800a6e596ca201
Shishir <shishirmax@gmail.com>
2017-03-24 18:15:18 +0530

    Added code to display from DB

---
+++ RHList/DisplayFromDB.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DisplayFromDB.aspx.cs" Inherits="RHList.DisplayFromDB" %>
+
+<%@ Register Assembly="Telerik.Web.UI" Namespace="Telerik.Web.UI" TagPrefix="telerik" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <telerik:RadScriptManager runat="server" ID="RadScriptManager1" />
+    <div>
+        <telerik:RadButton ID="RadButtonShowData" runat="server" Text="Display Data" OnClick="RadButtonShowData_Click"></telerik:RadButton>
+    </div>
+    <div>
+        <telerik:RadGrid ID="RadGrid1" runat="server"></telerik:RadGrid>
+    </div>
+    </form>
+</body>
+</html>
---
+++ RHList/DisplayFromDB.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RHList
+{
+    public partial class DisplayFromDB : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadButtonShowData_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from RH_List", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            RadGrid1.DataSource = ds;
+            RadGrid1.DataBind();
+        }
+    }
+}
